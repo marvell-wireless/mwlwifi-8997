cf306c79b840834341a1f99b8920327f9b49cee2
Ramesh Radhakrishnan <rramesh@marvell.com>
2017-06-15 12:24:24 +0530

    Change driver version from p11 -> p12

--- dev.h
+++ dev.h
@@ -30,7 +30,7 @@
 #include "pfu.h"
 
 #define MWL_DRV_NAME     KBUILD_MODNAME
-#define MWL_DRV_VERSION	 "P11-20170531"
+#define MWL_DRV_VERSION	 "P12-20170615"
 
 
 /* Map to 0x80000000 (Bus control) on BAR0 */
